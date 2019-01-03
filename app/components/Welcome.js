4f4130a233ab975aa57487a36bf32817a1ca6916
Guli <gulikholmatova@MacBook-Air-Guli.local>
2019-01-02 21:02:40 -0500

    Minor changes to HTML & CSS

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const welcome = () => {
-    return (
-        <div>
-            <main>
-                <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div>
+      <main>
+        <h1>Welcome to the Class Manager</h1>
+        <h2>Learn more</h2>
+        <img src="public/campus.jpeg" />
+      </main>
+    </div>
+  );
+};
 
 export default welcome;
