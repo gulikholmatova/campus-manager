9efc85b20c8d48a930269055d89b77e3e44d71e0
Guli <gulikholmatova@MacBook-Air-Guli.local>
2019-01-03 16:31:15 -0500

    Added CSS styling

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -3,11 +3,8 @@ import React from 'react';
 const welcome = () => {
   return (
     <div>
-      <main>
-        <h1>Welcome to the Class Manager</h1>
-        <h2>Learn more</h2>
-        <img src="public/campus.jpeg" />
-      </main>
+      <h1>Welcome to the Class Manager</h1>
+      <img className="carousel-image" src="campus.jpeg" />
     </div>
   );
 };
