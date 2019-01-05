6421f35e2fdbbcef06e28011bc470e61fa14cabc
Guli <gulikholmatova@MacBook-Air-Guli.local>
2019-01-05 12:13:22 -0500

    Added className to each form for a single item view component

--- app/components/SingleCampus.js
+++ app/components/SingleCampus.js
@@ -2,82 +2,92 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import UpdateCampusForm from './UpdateCampusForm'
+import UpdateCampusForm from './UpdateCampusForm';
 
-import { fetchCampus } from '../reducers/campus'
+import { fetchCampus } from '../reducers/campus';
 
 export class SingleCampus extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-    componentDidMount() {
-        const id = Number(this.props.match.params.campusId);
-        this.props.fetchCampus(id)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    const id = Number(this.props.match.params.campusId);
+    this.props.fetchCampus(id);
+  }
 
-    handleClick() {
-        this.setState({
-            active: !this.state.active
-        })
-    }
+  handleClick() {
+    this.setState({
+      active: !this.state.active,
+    });
+  }
 
-    render() {
-        const campus = this.props.campus
-        const { name, imageUrl, address, description, students } = campus
+  render() {
+    const campus = this.props.campus;
+    const { name, imageUrl, address, description, students } = campus;
 
-        return (
-            <div>
-                <h3>Campus Information:</h3>
-                <button type="button" onClick={this.handleClick}>Update information</button>
-                {this.state.active && <UpdateCampusForm />}
-                {campus &&
-                    <ul>
-                        <li>Campus name: {name} </li>
-                        <li>Campus image: <img src={imageUrl} /> </li>
-                        <li>Campus address: {address} </li>
-                        <li>Campus description: {description} </li>
-                        <li>Students on this campus: {
-                            students ?
-                                <ol>
-                                    {students.map((student) => {
-                                        return (<li key={student.id}><Link to={`/students/${student.id}`}> {student.firstName} {student.lastName} </Link></li>)
-                                    })}
-                                </ol> :
-                                "Currently no students study at this campus."
-                        }
-                        </li>
-
-                    </ul>
-                }
-            </div>
-        )
-    }
+    return (
+      <div className="list">
+        <h3>Campus Information:</h3>
+        <button type="button" onClick={this.handleClick}>
+          Update information
+        </button>
+        {this.state.active && <UpdateCampusForm />}
+        {campus && (
+          <ul>
+            <li>Campus name: {name} </li>
+            <li>
+              Campus image: <img src={imageUrl} />{' '}
+            </li>
+            <li>Campus address: {address} </li>
+            <li>Campus description: {description} </li>
+            <li>
+              Students on this campus:{' '}
+              {students ? (
+                <ol>
+                  {students.map(student => {
+                    return (
+                      <li key={student.id}>
+                        <Link to={`/students/${student.id}`}>
+                          {' '}
+                          {student.firstName} {student.lastName}{' '}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ol>
+              ) : (
+                'Currently no students study at this campus.'
+              )}
+            </li>
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 // Connecting to redux store:
 
 function mapStateToProps(state) {
-    return {
-        campus: state.campus
-    }
+  return {
+    campus: state.campus,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        fetchCampus: (id) => {
-            const thunk = fetchCampus(id);
-            dispatch(thunk);
-        }
-    }
+  return {
+    fetchCampus: id => {
+      const thunk = fetchCampus(id);
+      dispatch(thunk);
+    },
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleCampus);
-
-
-
-
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SingleCampus);
