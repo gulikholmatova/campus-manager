9239f0a233478a8476981ef66f1aade3ed0ab066
Guli <gulikholmatova@MacBook-Air-Guli.local>
2019-01-04 21:49:27 -0500

    Correction to scripts added for Heroku

--- server/index.js
+++ server/index.js
@@ -1,31 +1,42 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const path = require('path')
-const volleyball = require('volleyball')
-
-const app = express()
+const express = require('express');
+const path = require('path');
+const volleyball = require('volleyball');
+const { db } = require('./db');
+const app = express();
 
 // logging middleware
-app.use(volleyball)
+app.use(volleyball);
 
 // body parsing middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // static middleware
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api', require('./api')) // include our routes!
+app.use('/api', require('./api')); // include our routes!
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-}) // Send index.html for any other requests
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+}); // Send index.html for any other requests
 
 // error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error')
-})
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
 
-module.exports = app
+// Setting up a Port:
+const PORT = 4000;
+
+db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+  .then(() => {
+    console.log('db synced');
+    app.listen(PORT, () =>
+      console.log(`studiously serving silly sounds on port ${PORT}`)
+    );
+  });
+
+module.exports = app;
